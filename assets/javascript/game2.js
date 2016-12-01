0b79900710e6624f0819ddbe0c2e62bfcdb6342e
Meredith Erb <mereditherb@merediths-mbp.attlocal.net>
2016-11-30 20:52:54 -0500

    working hangman push

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,114 +1,92 @@
 
 var	currentWord = "",
-	userGuesses = "",  //Not sure if need this var
-	wrongGuesses = [], //use wrongGuesses.push("test"); when needing to add
+	userGuesses = "",  
+	wrongGuesses = "", 
 	totalGuesses = 0,
 	totalWins = 0,
-	letterUsed = false;
 	sepWord = "",
 	dashWord = "",
-	currentInProgress = "";
-var wordGroup = ["lion king", "aladdin", "mulan", "beauty and the beast", "frozen"];
-
-//currentInProgress as variable for .length didn't change it either
-if (currentWord.length <= 5) {
-		totalGuesses = 9;
-	} else if (currentWord.length > 5 && currentWord.length<= 10) {
-		totalGuesses = 14;
-	} else {
-		totalGuesses = 19;
-	}
-document.getElementById("totalGuesses").textContent = totalGuesses;
-
-// console.log(currentInProgress);
-// console.log(dashWord);
-// console.log(sepWord);
+	currentInProgress = "",
+	checkWord = false;
+var wordGroup = ["lion-king", "aladdin", "mulan", "beauty-and-the-beast", "frozen", "bambi", "snow-white", "cinderella", "pinocchio", "dumbo", "pocahontas", "the-little-mermaid"];
 
 //spacebar controls the refreshing of the chosen word
 document.onkeyup = function(event){
 
-    if(event.keyCode == 32){
-		currentWord = wordGroup[Math.floor(Math.random() * wordGroup.length)];
-    	
-      //build currentInProgress variable and fill with dashes
-	  sepWord = currentWord.split("");
-	  dashWord = currentWord.replace(/\w/g, "-");
-	  currentInProgress = dashWord.split("");
-
-	  document.getElementById("currentInProgress").textContent = currentInProgress;
-
-    }
-    else {
+    if(event.keyCode >= 65 && event.keyCode <= 91){
        	userGuesses = event.key;
        	userGuess();
+	  document.getElementById("wrongGuesses").textContent = wrongGuesses;
 
-    	// console.log(userGuesses);
+    } else if (event.keyCode === 32 && !checkWord) {
+	  currentWord = wordGroup[Math.floor(Math.random() * wordGroup.length)];
+	  wrongGuesses = "";
+document.getElementById("wrongGuesses").textContent = wrongGuesses;
+
+      //build currentInProgress variable and fill with dashes
+	  sepWord = currentWord.split("");
+	  dashWord = currentWord.replace(/\w/g, "_");
+	  currentInProgress = dashWord.split("");
+document.getElementById("currentInProgress").textContent = currentInProgress.join(" ");
+	
+	// console.log(currentInProgress);
+	// console.log(dashWord);
+	// console.log(sepWord);
+	totalGuesses = (Math.ceil(1.5 * currentWord.length));
+
+ 	document.getElementById("totalGuesses").textContent = totalGuesses;
     } 
-}
+      
+}//end of onkeyup 
 
 //code for user correct guess 
 function userGuess() {
-	
-  for (var i=0; i < currentWord.length; i++) {
 
+  var letterWrong = 0;	
+
+  for (var i=0; i < currentWord.length; i++) {
 	if (userGuesses === sepWord[i].toLowerCase()){
 		currentInProgress.splice(i, 1, userGuesses);
-		// console.log(currentInProgress);
-	}
-
-	else if (currentInProgress === currentWord){
-		//if all dashes gone, you win! add to total wins
-
-		alert("You Win!");
-		// add code to hold onto wrongGuesses and subtract from total allowed
-
-	}
-	
-	// add else? can I have the wrongGuesses go here? if it's not in the currentInProgress it goes here and writes out to page and reduces totalGuesses by one. 
-	else if (userGuesses === wrongGuesses){
-		alert("Letter has been used, select a different letter");
-
+		
 	}
 	else {
-		wrongGuesses
+		letterWrong +=1;
 	}
-  }		
+  } //end of for loop	
 
-document.getElementById("currentInProgress").textContent = currentInProgress;
-document.getElementById("wrongGuesses").textContent = wrongGuesses; 
+  if (letterWrong === currentWord.length) {
+	wrongGuesses = wrongGuesses.concat(userGuesses);
+	totalGuesses --;
+		//decrement total guesses by 1
+document.getElementById("totalGuesses").textContent = totalGuesses;
+	}
+document.getElementById("currentInProgress").textContent = currentInProgress.join(" ");
+document.getElementById("wrongGuesses").textContent = wrongGuesses;
 
+checkForStatus();
+} //end of userGuess function
+
+function checkForStatus() {
+	if (totalGuesses === 0) {
+		playLosingSound();
+		alert("You lose");
+		checkWord = false;
+	} else if (currentInProgress.indexOf("_") > -1) {
+		checkWord = true;
+	} else {
+		playWinningSound();
+		alert("You Win! Press OK then spacebar to continue");
+		totalWins ++;
+		checkWord = false;
+	}
+document.getElementById("totalWins").textContent = totalWins;
 }
 
-//letterUsed is set to false so not my letter used literally
-
-	
-
-
-
-// document.onkeyup = function(event){
-
-
-// if (userGuesses == wrongGuesses) {
-// 	alert ("letter has been used");
-// }
-// else {
-// 	for (var i = 0; i < currentInProgress.length; i++) {
-// 		if (userGuesses === currentInProgress[i]){
-// 			//replace letter in string
-// 			letterUsed == true;
-// 		}
-// 			totalGuesses ++;
-// 	}
-// }
-// 	document.getElementById("currentInProgress").textContent = currentInProgress;
-// }
+function playWinningSound() {
+		document.getElementById("soundWin").play();
+	}
+function playLosingSound() {
+		document.getElementById("soundLoss").play();
+	}
 
 
-
-
-
-// line added in from class to maybe help?
-// currentInProgress.split("");
-
-// for (var i=0; i<currentInProgress.length; i++);
-// 	console.log(currentInProgress[i]);
